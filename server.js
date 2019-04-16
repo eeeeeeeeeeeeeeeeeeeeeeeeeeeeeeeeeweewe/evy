db36b0b7bd68010ca09af4e718c83b43c29fccca
eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeweewe <49655863+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeweewe@users.noreply.github.com>
2019-04-16 12:55:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync('index.html');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(index);
-}).listen(80);
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(80, function(){
+    console.log('Server running on 80...');
+});
